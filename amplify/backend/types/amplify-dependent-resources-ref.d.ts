52ac1e8f4b2308284afd272d5863a40508fa72d6
Simmigon Flagg <simmigon.flagg@gmail.com>
2022-05-11 12:00:31 -0400

    Added Auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "kaipher36ac53cb": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div >
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <br />
 
+        </div>
+      )}
+    </Authenticator>
+  );
 }
 
 export default App;
